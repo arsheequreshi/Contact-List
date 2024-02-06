1b45931dd5a54ea43e684cf38ac2514b61574632
arsheequreshi <arshee98@gmail.com>
2024-02-06 19:53:57 +0530

    Add files via upload

---
+++ ContactsList-Coding_Ninjas-main/src/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ContactList } from './components/data';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+window.alert("Hey👋 this project is created by Arshee!")
+
+root.render(
+  <StrictMode>
+    <ContactList />
+  </StrictMode>
+);
